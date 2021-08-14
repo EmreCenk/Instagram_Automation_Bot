f61f401ee9ed3c9d1067958513eb6bdf69df824f
Emre Cenk <emrecenk9@gmail.com>
2021-08-13 22:43:17 -0400

    added a timeout to the scrolldown function, the new user folder is created using an absoloute path

--- Static_Functions/Writing_Analysis_Files.py
+++ Static_Functions/Writing_Analysis_Files.py
@@ -4,7 +4,6 @@ path=r"C:\Users\Murat\Desktop\Emre\Python\Instabot_2.4\Analysis_of_People\ "
 def write_list_for_new_user(user,followers,following,no_follow_back):
     from datetime import datetime
     import os
-    os.mkdir(user)
     cmonman = path
     cmonman = cmonman[0:-1]
     backslash = "\ "
@@ -17,6 +16,8 @@ def write_list_for_new_user(user,followers,following,no_follow_back):
         else:
             newname = newname + i
     newname = user + newname
+
+    os.mkdir(cmonman + user)
     folder = open(cmonman + user + backslash + newname + ".txt", "a")
     folder.write("followers" + "=" + str(followers) + "\n")
     folder.write("\n" + "following" + "=" + str(following) + "\n\n")
--- bot_functions/utils.py
+++ bot_functions/utils.py
@@ -2,9 +2,9 @@
 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
+from time import sleep, perf_counter
 
-def scrolldown(self, numpeople, classname="FPmhX"):
+def scrolldown(self, numpeople, classname="FPmhX", time_out_seconds = 15):
     """This function is made to scroll down the list of followers/following for a person. It can be tweaked to
     become a regular automatic scroller.
     After scrolling all the way down, it return an array of the people that it has scrolled through."""
@@ -24,15 +24,23 @@ def scrolldown(self, numpeople, classname="FPmhX"):
         thingtodo.send_keys(Keys.TAB).perform()
 
         now = self.browser.find_elements_by_class_name(classname)
-        while len(now) <= numpeople - 4:
+        start = perf_counter()
+        starting_len = 0
+        while len(now) <= numpeople - 4 and perf_counter() - start < time_out_seconds:
             now = self.browser.find_elements_by_class_name(classname)
             thingtodo.send_keys(Keys.END).perform()
             sleep(self.scrollsleep)
+            if len(now) != starting_len:
+                starting_len = len(now)
+                start = perf_counter()
 
         now = self.browser.find_elements_by_class_name(classname)
         final = []
         for i in now:
             final.append(i.text)
+
+        if len(final) != numpeople:
+            print("We were only able to extract " + str(len(final)) + " people for the user instead of " + str(numpeople))
 
         return final
 
